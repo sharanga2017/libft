7a141c6e28ff323a395ad1197ffffc3558e7c363
Serguei Khan <33214783+sharanga2017@users.noreply.github.com>
2021-11-26 00:27:24 +0100

    Create ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub;
+	unsigned int	i;
+
+	i = 0;
+	if (!s || !(sub = (char *)ft_memalloc(len + 1)))
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start];
+		i++;
+		start++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
